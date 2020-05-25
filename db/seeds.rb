35baab5c9235c2a716da14df800c23028d0c02c6
github username <justinzhao265@gmail.com>
2020-05-25 02:55:41 -0400

    adjust cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Card.create(name: 'big dmg', description: 'Deal 10 damage', effect_type: 'damage', effect: 10, special: true)
+Card.create(name: 'big dmg', description: 'Deal 8 damage', effect_type: 'damage', effect: 8, special: true)
 Card.create(name: '2x', description: 'next card x2 effect', effect_type: 'buff', effect: 2, special: true)
 Card.create(name: 'lil dmg', description: 'Deal 5 damage', effect_type: 'damage', effect: 5, special: false)
 Card.create(name: 'heal', description: 'Heal 3 HP', effect_type: 'heal', effect: 3, special: false)
 Card.create(name: 'vamp', description: 'Deal 2 damage and heal 2 HP', effect_type: 'vamp', effect: 2, special: false)
 
-Enemy.create(name: 'Slime', hp: 100, deck: [2,2,2,3])
-Enemy.create(name: 'Kobold', hp: 200, deck: [1,2,3,4])
-Enemy.create(name: 'Orc', hp: 300, deck: [1,1,4,4])
+Enemy.create(name: 'Slime', hp: 50, deck: [3,3,3,4])
+Enemy.create(name: 'Kobold', hp: 70, deck: [3,3,4,5])
+Enemy.create(name: 'Orc', hp: 100, deck: [1,3,3,3])
+Enemy.create(name: 'Lich King', hp: 150, deck: [2,3,5,5])

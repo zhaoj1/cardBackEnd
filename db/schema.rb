0295c7ab7cf5db660690254db7cb633b2db2d671
github username <justinzhao265@gmail.com>
2020-06-01 16:26:55 -0400

    added cards

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_10_201220) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.string "name"
     t.string "description"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Card.create(name: 'big dmg', description: 'Deal 10 damage', effect_type: 'damage', effect: 8, special: true)
+Card.create(name: 'big dmg', description: 'Deal 10 damage', effect_type: 'damage', effect: 10, special: true)
 Card.create(name: 'double', description: 'Next card x2 effect', effect_type: 'buff', effect: 0, special: true)
 Card.create(name: 'guard', description: 'Block enemy card', effect_type: 'guard', effect: 0, special: true)
-Card.create(name: 'lil dmg', description: 'Deal 4 damage', effect_type: 'damage', effect: 5, special: false)
-Card.create(name: 'heal', description: 'Heal 8 HP', effect_type: 'heal', effect: 3, special: false)
-Card.create(name: 'vamp', description: 'Deal 3 damage and heal 3 HP', effect_type: 'vamp', effect: 2, special: false)
+Card.create(name: 'lil dmg', description: 'Deal 4 damage', effect_type: 'damage', effect: 4, special: false)
+Card.create(name: 'heal', description: 'Heal 8 HP', effect_type: 'heal', effect: 8, special: false)
+Card.create(name: 'vamp', description: 'Deal 3 damage and heal 3 HP', effect_type: 'vamp', effect: 3, special: false)
+Card.create(name: 'dagger', description: 'Deal 2 damage and redraw 1 random card from graveyard', effect_type: 'draw', effect: 0, special: false)
+Card.create(name: 'sns', description: 'Deal 2 damage and block 2 damage from enemy card', effect_type: 'sns', effect: 2, special: false)
 
 Enemy.create(name: 'Slime', hp: 30, deck: [4,4,4,5])
 Enemy.create(name: 'Kobold', hp: 50, deck: [1,4,5,6])
